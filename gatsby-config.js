18a914acc8426281fc52b10245b91df2a7d89f87
Sean Ho <seanho96@yahoo.com>
2021-03-13 19:37:29 +0800

    fix: changed GTM id

--- gatsby-config.js
+++ gatsby-config.js
@@ -150,7 +150,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: 'UA-45666519-2',
+        trackingId: 'UA-192091586-1',
       },
     },
   ],
